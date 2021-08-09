35519a4fb7e7dedd45c9897377f5fd7e10470afb
rscholtelubberink <80954753+rscholtelubberink@users.noreply.github.com>
2021-08-09 10:44:02 +0200

    Update enable.ps1
    Encrypting / Account object info

--- enable.ps1
+++ enable.ps1
@@ -78,7 +78,7 @@ if (-not($dryRun -eq $true)) {
 
         $splatParams = @{
             Uri     = "$($c.BaseUrl)/webservices/$($c.Customer)/rest/functions/createupdate/ImportItem"
-            Body    = $body
+            Body    = ([System.Text.Encoding]::UTF8.GetBytes($body))
             Headers = $headers
             Method  = 'POST'
         }
@@ -119,6 +119,12 @@ $result = [PSCustomObject]@{
     Success   = $success
     Account   = $account
     AuditLogs = $auditLogs
+    
+        ExportData = [PSCustomObject]@{
+        displayName = $account.id;
+        userName = $account.id;
+        email = $account.emailAddress;
+    };
 }
 
 Write-Output $result | ConvertTo-Json -Depth 10
