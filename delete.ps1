5aa47d8d627e4486f40cdfd5b1f4ac0e3a501178
rscholtelubberink <80954753+rscholtelubberink@users.noreply.github.com>
2021-08-09 10:45:38 +0200

    Create delete.ps1
    Initial commit

---
+++ delete.ps1
@@ -0,0 +1,130 @@
+#########################################################
+# HelloID-Conn-Prov-Target-Spacewell-Axxerion-Disable
+#
+# Version: 1.0.0.0
+##########################################################
+$VerbosePreference = "Continue"
+
+# Initialize default value's
+$c = $configuration | ConvertFrom-Json
+$aRef = $accountReference | ConvertFrom-Json
+$p = $person | ConvertFrom-Json
+$success = $false
+$auditLogs = New-Object Collections.Generic.List[PSCustomObject]
+
+$email = $p.Accounts.MicrosoftActiveDirectory.Mail
+
+$account = [PSCustomObject]@{
+    id           = $p.ExternalId;
+    userName     = $p.ExternalId;
+    givenName    = $p.Name.Nickname;
+    familyName   = $p.Name.FamilyName;
+    cost_center =  $P.PrimaryContract.CostCenter.Name
+    job_title = $p.PrimaryContract.Title.Name;
+    emailAddress = $email;
+}
+
+
+#region helper functions
+function Resolve-HTTPError {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,
+            ValueFromPipeline
+        )]
+        [object]$ErrorObject
+    )
+    process {
+        $HttpErrorObj = @{
+            FullyQualifiedErrorId = $ErrorObject.FullyQualifiedErrorId
+            InvocationInfo        = $ErrorObject.InvocationInfo.MyCommand
+            TargetObject          = $ErrorObject.TargetObject.RequestUri
+            StackTrace            = $ErrorObject.ScriptStackTrace
+        }
+        if ($ErrorObject.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') {
+            $HttpErrorObj['ErrorMessage'] = $ErrorObject.ErrorDetails.Message
+        } elseif ($ErrorObject.Exception.GetType().FullName -eq 'System.Net.WebException') {
+            $stream = $ErrorObject.Exception.Response.GetResponseStream()
+            $stream.Position = 0
+            $streamReader = New-Object System.IO.StreamReader $Stream
+            $errorResponse = $StreamReader.ReadToEnd()
+            $HttpErrorObj['ErrorMessage'] = $errorResponse
+        }
+        Write-Output $HttpErrorObj
+    }
+}
+#endregion
+
+if (-not($dryRun -eq $true)) {
+    try {
+        Write-Verbose "Deleting Axxerion user '$($p.DisplayName)'"
+        $clobMBValue = @{
+            action = 'Account Revoke'
+            body = @{
+                id = $account.id
+            }
+        } | ConvertTo-Json
+
+        $Body = @{
+            datasource  = 'HelloID'
+            clobMBValue = $clobMBValue
+        } | ConvertTo-Json
+
+        $authorization = "$($c.UserName):$($c.Password)"
+        $base64Credentials = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($authorization))
+        $headers = @{
+            "Authorization" = "Basic $base64Credentials"
+        }
+
+        $splatParams = @{
+            Uri     = "$($c.BaseUrl)/webservices/$($c.Customer)/rest/functions/createupdate/ImportItem"
+            Body    = ([System.Text.Encoding]::UTF8.GetBytes($body))
+            Headers = $headers
+            Method  = 'POST'
+        }
+        $disableResponse = Invoke-RestMethod @splatParams
+        if ($disableResponse.response -eq 'none'){
+            $success = $false
+            $auditLogs.Add([PSCustomObject]@{
+                Action  = 'DeleteAccount'
+                Message = $disableResponse.errorMessage
+                IsError = $true
+            })
+        } elseif ($disableResponse.id){
+            $success = $true
+            $auditLogs.Add([PSCustomObject]@{
+                Message = "Account for '$($p.DisplayName)' successfully deleted with id: '$($aRef)'"
+                IsError = $false
+            })
+        }
+    } catch {
+        $ex = $PSItem
+        $success = $false
+        if ( $($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
+            $errorObject = Resolve-HTTPError -Error $ex
+            $auditMessage = "Could not delete user account for '$($p.DisplayName)', Error $($errorObject.ErrorMessage)"
+        } else {
+            $auditMessage = "Could not delete user account for '$($p.DisplayName)', Error: $($ex.Exception.Message)"
+        }
+        $auditLogs.Add([PSCustomObject]@{
+            Action  = "DeleteAccount"
+            Message = $auditMessage
+            IsError = $true
+        })
+        Write-Verbose $auditLogs.Message
+    }
+}
+
+$result = [PSCustomObject]@{
+    Success   = $success
+    Account   = $account
+    AuditLogs = $auditLogs
+
+    ExportData = [PSCustomObject]@{
+        displayName = $account.id;
+        userName = $account.id;
+        email = $account.emailAddress;
+    };
+}
+
+Write-Output $result | ConvertTo-Json -Depth 10
